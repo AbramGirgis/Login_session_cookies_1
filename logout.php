36ea8c8c9f51505e51c75d85246d163f18d3e551
AbramGirgis <girgis.abram@gmail.com>
2022-09-12 19:10:10 -0400

    Remember Me

--- logout.php
+++ logout.php
@@ -2,5 +2,9 @@
 session_start();
 session_reset();
 session_destroy();
+// To destroy the cookie:
+//if (isset($_COOKIE['email'])) {
+//    $email = $_COOKIE['email'];
+//    setcookie('email',$email,time()-1);
 echo "You have successfully logged out! <br><br> Click here to <a href='login.php'>login again</a>";
 
